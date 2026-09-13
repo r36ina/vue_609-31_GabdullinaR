3c9983cc29e35f12f8406765dd3ad187abc444b9
regina <rgabdullina@ya.ru>
2026-09-13 19:22:35 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyComponent from './components/MyComponent.vue';
+import MyNewComponent from './components/MyNewComponent.vue';
+</script>
+
+<template>
+  <MyComponent></MyComponent>
+  <MyNewComponent></MyNewComponent>
+</template>
+
+<style scoped></style>
---
+++ src/components/MyNewComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {ref} from "vue";
+const count = ref(0);
+function increment() {
+    count.value++;
+}
+</script>
+
+<template>
+    <h1>Composition API</h1>
+    <h2>{{ count }}</h2>
+    <button @click="increment">+</button>
+</template>
